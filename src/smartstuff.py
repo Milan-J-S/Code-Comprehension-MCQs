b301891aad9441e406670db5b8bde49c623207c4
Milan J S <milan.j.srinivas@gmail.com>
2019-02-27 15:55:46 +0000

    Replace smartstuff.py

--- src/smartstuff.py
+++ src/smartstuff.py
@@ -9,9 +9,10 @@ from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 import numpy as np
 
+from nltk.tokenize import word_tokenize
 import os
 
-file_path = 'C:\PES\8TH SEM\semi-intelligent-discussion-forum-for-code-master\semi-intelligent-discussion-forum-for-code-master\src\code'
+file_path = 'code'
 
 codes = []
 comments = []
@@ -20,15 +21,22 @@ comments_vector = {}
 
 for root,dirs,files in os.walk(file_path):
     for fl in files:
-        codes.append(open('code/'+fl).read())
+        code = open('code/'+fl).read()
+        code_tensor =  np.asarray(word_tokenize(code)[:1000])
+        print(len(code_tensor), "--->")
+        if(len(code_tensor) < 1000):
+            code_tensor = np.pad(code_tensor, (0, 1000 - len(code_tensor)) , 'constant' )
+        print(len(code_tensor))
+        codes.append(code_tensor)
         comment = open('comments/'+fl).read()
         for c in comment.split():
             vocab.add(c)
         comments.append(comment)
 
 
-print(codes)
-print(comments)
+# print(comments)
+
+print(np.asarray(codes).shape)
 
 i=0
 
@@ -45,19 +53,23 @@ for item in comments:
         comment_tensor.append(comment)
     comments_tensor.append(comment_tensor)
 
-print(comments_tensor)
+print(np.asarray(comments_tensor).shape)
 
 model = Sequential()
 # model.add(Input(shape = (2000,)))
 
-model.add(Embedding(10, 100, input_length=50))
-model.add(LSTM(100, return_sequences=True))
-model.add(LSTM(100, return_sequences=True))
+model.add(Embedding(10, 100, input_length=1000))
+model.add(LSTM(500, return_sequences=True))
+model.add(LSTM(500, return_sequences=True))
 model.add(TimeDistributed(Dense(len(vocab))))
 
 model.compile(loss = 'categorical_crossentropy', optimizer='RMSProp', metrics = ['accuracy'])
 
 print(model.summary())
+
+model.fit(np.asarray(codes), np.asarray(comments_tensor), epochs=2, verbose = 1)
+
+
 
 
 
