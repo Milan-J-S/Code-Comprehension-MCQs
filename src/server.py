6eaa93621f12f9843991312208dc465ba1827daf
Milan <milan.j.srinivas@gmail.com>
2019-03-09 13:53:32 +0530

    Minor fixes

--- src/server.py
+++ src/server.py
@@ -1,27 +1,30 @@
-from flask import Flask, request, jsonify,session, g, redirect, url_for, abort, \
-    render_template, flash , send_from_directory
+from flask import Flask, request, jsonify, session, g, redirect, url_for, abort, \
+    render_template, flash, send_from_directory
 
 from os import listdir
 
 app = Flask(__name__)
+
+
 # CORS(app)
 
 @app.route("/")
 def start():
-   return render_template('codeUpload.html')
+    return render_template('codeUpload.html')
 
-@app.route("/showCode",methods=["GET","POST"])
+
+@app.route("/showCode", methods=["GET", "POST"])
 def showCode():
     filename = request.args['filebirthname']
-   
-    filename = 'static/data/'+filename
+
+    filename = 'static/data/' + filename
     print(filename)
-    with open(filename) as f : 
-        return render_template('codeView.html', data = str(f.read())) 
+    with open(filename) as f:
+        return render_template('codeView.html', data=str(f.read()))
 
-   
+    # @app.route("/getFile", methods=["GET","POST"])
 
-# @app.route("/getFile", methods=["GET","POST"])
+
 # def getFile():
 #     with open('static/data/test.c') as f : 
 #         return jsonify(data = str(f.read()), mode = "text/x-csrc" )
@@ -35,9 +38,9 @@ def showAll():
         di['title'] = f
         items.append(di)
     print(items)
-    return render_template('showResources.html', items = items) 
+    return render_template('showResources.html', items=items)
+
 
 if __name__ == '__main__':
-   
-   app.config['TEMPLATES_AUTO_RELOAD'] = True
-   app.run(host = '0.0.0.0')
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(host='0.0.0.0')
