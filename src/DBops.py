5daae08e5614d814925c5c183ba8a81479356713
Michelle Roy <michellemroy96@gmail.com>
2019-03-10 10:29:36 +0000

    Update src/templates/codeUpload.html, src/DBops.py, src/server.py files

--- src/DBops.py
+++ src/DBops.py
@@ -4,7 +4,10 @@ con = sqlite3.connect("database.db");
 # con.execute("CREATE TABLE Convos (filename TEXT, id NUMBER, comment TEXT, user TEXT)")
 # print("Table created succesfully");
 
-con.execute("CREATE TABLE Codes (poster TEXT, filename TEXT, description TEXT, lang TEXT)")
-print("Table created succesfully");
+# con.execute("CREATE TABLE Codes (poster TEXT, filename TEXT, description TEXT, lang TEXT)")
+# print("Table created succesfully");
+
+con.execute("CREATE TABLE Login ( email TEXT, pw TEXT )")
+print("Table created succesfully")
 
 con.close()
