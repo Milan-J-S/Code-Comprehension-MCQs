3c8f209a37a22bfdb2116256fec25528ce6da9ff
Milan <milan.j.srinivas@gmail.com>
2019-03-25 22:14:23 +0530

    Add Table for tags

--- src/DBops.py
+++ src/DBops.py
@@ -13,4 +13,12 @@ con.execute("DROP TABLE Login")
 con.execute("CREATE TABLE Login ( email TEXT, pw TEXT )")
 print("Table created succesfully")
 
+con.execute("DROP TABLE Tags")
+con.execute("CREATE TABLE Tags( tag TEXT )")
+print("Table created succesfully")
+
+con.execute("DROP TABLE CodeTags")
+con.execute("CREATE TABLE CodeTags( code TEXT references Codes(filename), tag TEXT references Tags(tag))")
+print("Table created succesfully")
+
 con.close()
