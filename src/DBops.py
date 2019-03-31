98a3358ca981b82b4f03267185d8a4735c844678
Milan <milan.j.srinivas@gmail.com>
2019-03-31 16:13:25 +0530

    Add search by tag

--- src/DBops.py
+++ src/DBops.py
@@ -1,30 +1,28 @@
 import sqlite3
 con = sqlite3.connect("database.db")
 
-# con.execute("DROP TABLE Convos")
-# con.execute("CREATE TABLE Convos (filename TEXT, id NUMBER, comment TEXT, user TEXT)")
-# print("Table created succesfully")
-#
-# con.execute("DROP TABLE Codes")
-# con.execute("CREATE TABLE Codes (poster TEXT, filename TEXT, description TEXT, lang TEXT)")
-# print("Table created succesfully")
-#
-# con.execute("DROP TABLE Login")
-# con.execute("CREATE TABLE Login ( email TEXT, pw TEXT )")
-# print("Table created succesfully")
-#
-# con.execute("DROP TABLE Tags")
-# con.execute("CREATE TABLE Tags( tag TEXT, PRIMARY KEY(tag) ) ")
-# print("Table created succesf ully")
+con.execute("DROP TABLE Convos")
+con.execute("CREATE TABLE Convos (filename TEXT, id NUMBER, comment TEXT, user TEXT)")
+print("Table created succesfully")
 
-con.execute("ALTER TABLE Tags REBUILD WITH (IGNORE_DUP_KEY = ON)")
+con.execute("DROP TABLE Codes")
+con.execute("CREATE TABLE Codes (poster TEXT, filename TEXT, description TEXT, lang TEXT)")
+print("Table created succesfully")
 
-#
-# con.execute("DROP TABLE CodeTags")
-# con.execute("CREATE TABLE CodeTags( code TEXT references Codes(filename), tag TEXT references Tags(tag))")
-# print("Table created succesfully")
+con.execute("DROP TABLE Login")
+con.execute("CREATE TABLE Login ( email TEXT, pw TEXT )")
+print("Table created succesfully")
 
-# con.execute("CREATE TABLE CodeViews( code TEXT references Codes(filename), user TEXT references Login(email), difficulty NUMERIC)")
-# print("Table created succesfully")
+con.execute("DROP TABLE Tags")
+con.execute("CREATE TABLE Tags( tag TEXT, PRIMARY KEY(tag) ) ")
+print("Table created succesf ully")
+
+con.execute("DROP TABLE CodeTags")
+con.execute("CREATE TABLE CodeTags( code TEXT references Codes(filename), tag TEXT references Tags(tag))")
+print("Table created succesfully")
+
+con.execute("DROP TABLE CodeViews")
+con.execute("CREATE TABLE CodeViews( code TEXT references Codes(filename), user TEXT references Login(email), difficulty NUMERIC)")
+print("Table created succesfully")
 
 con.close()
