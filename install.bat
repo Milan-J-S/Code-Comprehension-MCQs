fa3bb570dab30d2ecc6bf1e7083cb8c6aaf80210
Milan <milan.j.srinivas@gmail.com>
2019-04-26 21:47:00 +0530

    Add dependency install batch script

---
+++ install.bat
@@ -0,0 +1,11 @@
+pip install random
+pip install flask
+pip install sqlite 3
+pip install numpy
+pip install nltk
+pip install gensim
+pip install pickle
+pip install tensorflow
+pip install keras
+
+ECHO All dependencies installed
