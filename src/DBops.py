3af714032d22e8508bcab3f4c3c06114131b9c93
Milan J S <Milan.j.srinivas@gmail.com>
2019-12-09 05:33:01 +0530

    Update DBops.py

--- src/DBops.py
+++ src/DBops.py
@@ -2,7 +2,7 @@ import sqlite3
 
 con = sqlite3.connect("database.db")
 cur = con.cursor()
-cur.execute("DROP TABLE ModifiedDistractors")
-cur.execute("CREATE TABLE ModifiedDistractors ( user TEXT, timeDiff NUMBER, filename TEXT, option1 TEXT, option2 TEXT, option3 TEXT, originalOption1 TEXT, originalOption2 TEXT, originalOption3 TEXT )")
-cur.execute("CREATE TABLE CreatedDistractors ( user TEXT, timeDiff NUMBER, option1 TEXT, option2 TEXT, option3 TEXT, filename TEXT)")
+# cur.execute("DROP TABLE ModifiedDistractors")
+# cur.execute("CREATE TABLE ModifiedDistractors ( user TEXT, timeDiff NUMBER, filename TEXT, option1 TEXT, option2 TEXT, option3 TEXT, originalOption1 TEXT, originalOption2 TEXT, originalOption3 TEXT )")
+cur.execute("CREATE TABLE GoodCodes SELECT * FROM Codes;")
 con.commit()
