85484fc52ac24c207c82cbb7f1e42b70f73923ab
Milan J S <Milan.j.srinivas@gmail.com>
2020-01-18 11:06:02 +0530

    Update paper_server.py

--- src/paper_server.py
+++ src/paper_server.py
@@ -69,8 +69,8 @@ def convertToFilesDict(x):
     obj['filename'] = x[2]
     return obj
 
-group1 = [ "user_1", "user_3", "user_5", "user_7", "user_9" ]
-group2 = [ "user_2", "user_4", "user_6", "user_8", "user_10" ]
+group1 = [ 'user_2','user_3','user_5','user_6','user_8','user_11','user_12','user_13','user_14','user_15','user_19','user_21','user_22','user_23','user_25','user_27','user_33','user_34' ]
+group2 = [ 'user_1','user_4','user_7','user_10','user_16','user_17','user_18','user_20','user_24','user_26','user_28','user_29','user_30','user_31','user_32','user_35','user_36' ]
 
 workshop_codes = ['zqkerplfph', 'vqwfsnsyns', 'elurfearoz', 'zqwafyxmfa', 'etadekqyjf', 'nwdfdklgvt', 'mbsgtzqabz', 'rqejjolvzu', 'wmggdqsohi', 'qkrijbtmhw' ]
 
